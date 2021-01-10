86010a8becf3a0dcc57aa06e934b7e5c9523e3e7
Bobby Zia <mr.baberzia@yahoo.com>
2021-01-10 17:42:56 -0500

    Prepare for deployment

---
+++ README.md
@@ -0,0 +1,86 @@
+# StoreFront - An E-Commerce Platform
+
+![screenshot](https://github.com/bradtraversy/proshop_mern/blob/master/uploads/Screen%20Shot%202020-09-29%20at%205.50.52%20PM.png)
+
+## Features
+
+- Full featured shopping cart
+- Product reviews and ratings
+- Top products carousel
+- Product pagination
+- Product search feature
+- User profile with orders
+- Admin product management
+- Admin user management
+- Admin Order details page
+- Mark orders as delivered option
+- Checkout process (shipping, payment method, etc)
+- PayPal / credit card integration
+- Database seeder (products & users)
+
+## Usage
+
+### Env Variables
+
+Create a .env file in then root and add the following
+
+```
+NODE_ENV = development
+PORT = 5000
+MONGO_URI = your mongodb uri
+JWT_SECRET = 'abc123'
+PAYPAL_CLIENT_ID = your paypal client id
+```
+
+### Install Dependencies (frontend & backend)
+
+```
+npm install
+cd frontend
+npm install
+```
+
+### Run
+
+```
+# Run frontend (:3000) & backend (:5000)
+npm run dev
+
+# Run backend only
+npm run server
+```
+
+## Build & Deploy
+
+```
+# Create frontend prod build
+cd frontend
+npm run build
+```
+
+There is a Heroku postbuild script, so if you push to Heroku, no need to build manually for deployment to Heroku
+
+### Seed Database
+
+The following commands (defined in package.json "scripts") can be used to wipe the user and product data or overwirte it with sample data.
+
+```
+# Fill db with sample users and products
+npm run data:import
+
+# Destroy data completely
+npm run data:destroy
+```
+
+```
+Sample User Logins
+
+admin@example.com (Admin)
+123456
+
+john@example.com (Customer)
+123456
+
+jane@example.com (Customer)
+123456
+```
---
+++ backend/data/products.js
@@ -0,0 +1,137 @@
+const products = [
+  {
+    name: 'Massdrop CTRL Mechanical Keyboard',
+    image: '/images/keyboard.jpg',
+    description:
+      'The Massdrop CTRL is a tenkeyless mechanical keyboard that straddles the line between productivity and play. The highlight of the CTRL is its hot-swappable key switches, which provide an effortlessly customizable keyboard experience.',
+    brand: 'Massdrop',
+    category: 'Electronics',
+    price: 199.99,
+    countInStock: 4,
+    rating: 5,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple AirPods Pro',
+    image: '/images/airpodsPro.jpg',
+    description:
+      'AirPods Pro have been designed to deliver Active Noise Cancellation for immersive sound. Their Transparency mode lets you hear your surroundings and a customisable fit for all-day comfort keeps your tunes playing. Just like AirPods, AirPods Pro connect like magic to your iPhone or Apple Watch. Plus, they’re ready to use straight out of the case.',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 249.99,
+    countInStock: 4,
+    rating: 4,
+    numReviews: 0,
+  },
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Playstation 5',
+    image: '/images/ps5.jpg',
+    description:
+      'Unleash new gaming possibilities beyond your wildest imagination with the PlayStation 5 console. Get lightning-fast loading times on the PS5 with an ultra-high speed SSD, deeper immersion with support for haptic feedback, adaptive triggers, 3D Audio, 4K and 8K output, and an all-new generation of incredible PlayStation games.',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 499.99,
+    countInStock: 8,
+    rating: 3.5,
+    numReviews: 0,
+  },
+
+  {
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+  {
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 0,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Google Dot',
+    image: '/images/googleHomeMini.png',
+    description:
+      'Google Home Mini is Google Assistant anywhere you want it. Ask it questions. Tell it to do things. It is your own google, always ready to help.',
+    brand: 'Google',
+    category: 'Electronics',
+    price: 69.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 0,
+  },
+  {
+    name: 'Beats Pill+ Speaker',
+    image: '/images/beatsPill.jpg',
+    description:
+      'From the office to the backyard, this Pill+ speaker by Beats delivers high quality music wherever you are. These wireless speakers deliver full, immersive sound wherever you choose. It has USB, lightning connectivity. Included in the box, you will also find a beats pill+ speaker, soft carrying case, 1.5m lightning to USB-a charging cable, 12.5w power adapter, quick start guide, warranty card.',
+    brand: 'Beats',
+    category: 'Electronics',
+    price: 199.99,
+    countInStock: 0,
+    rating: 3.7,
+    numReviews: 0,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
